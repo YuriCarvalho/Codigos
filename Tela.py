760dd801de5161ad47ca7fa55de090e136b8c25c
YuriCarvalho <yuricarvalho10@hotmail.com>
2023-03-07 21:33:06 -0300

    Update Tela.py

--- Tela.py
+++ Tela.py
@@ -3,11 +3,12 @@ import PySimpleGUI as sg
 #Layout
 sg.theme('Reddit')
 layout = [
-    [sg.Text('Quantidade de Tupla:'), sg.Input(key='tuplas', size=(20,1))],
-    [sg.Text('Quantidade de Páginas:'), sg.Input(key='páginas', size=(20,1))],
     [sg.Text('Tamanho da Página:'), sg.Input(key='tamanhop', size=(20,1))],
+    [sg.Text('Busca por Tupla:'), sg.Input(key='páginas', size=(19,1))],
+    [sg.Button('Search'), sg.Button('Cancel')],
+    [sg.Text('Quantidade Table Scan:'), sg.Input(key='qtdtable', size=(20,1))],
     [sg.Text('Table Scan:'), sg.Input(key='scan', size=(20,1))],
-    [sg.Button('Search'), sg.Button('Cancel')]
+    [sg.Button('Scan'), sg.Button('Cancel2')]
 ]
 
 #Janela 
@@ -16,8 +17,10 @@ janela = sg.Window('Tela do Usuário', layout)
 #Ler os eventos
 while True:
     eventos, valores = janela.read()
-    if eventos in (None, 'Cancel'):
+    if eventos in (None, 'Cancel', 'Cancel2'):
         break
             
     if eventos == 'Search':
-        print('ok')
+        print('Search')
+    if eventos == 'Scan':
+        print('Scan')
